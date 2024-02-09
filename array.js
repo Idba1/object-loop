ed57fa02c0ccf4db2dd996b1575a3d2e99c87c7e
Idba1 <moniraislamself@gmail.com>
2024-02-09 13:22:37 +0600

    array like object

---
+++ array.js
@@ -0,0 +1,14 @@
+const numbers = [22, 34, 4, 5, 66, 78, 89, 33, 55, 33];
+for(let number of numbers){
+    console.log(number);
+}
+
+
+const products = {
+    '0' : 19,
+    '1' : 16,
+    '2' : 11,
+    '3' : 151,
+    '4' : 20,
+}
+console.log(products);
